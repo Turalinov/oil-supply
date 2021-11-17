3c30e3d1ccbefdaaa0da0131b189a12337d3608d
Turalinov <rusltur_24@mail.ru>
2021-11-17 23:35:34 +0600

    header fix

---
+++ src/scripts/header.js
@@ -0,0 +1,29 @@
+(function() {
+  let last_known_scroll_position = 0;
+  let ticking = false;
+  let header = document.querySelector('.header');
+
+  function doSomething(scroll_pos) {
+    // Делаем что-нибудь с позицией скролла
+    console.log(scroll_pos)
+
+    if (scroll_pos > 5) {
+      header.classList.add('_active')
+    } else {
+      header.classList.remove('_active')
+    }
+  }
+
+  window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
+})()
